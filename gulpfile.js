15881ff8809d18da609e8179ef9314ee9493e311
Michael Vieira <mvieira@aureusnetwork.fr>
2015-08-04 19:41:54 +0100

    Modify Gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ var gulp = require('gulp')
 
 var minifyCss = require('gulp-minify-css');
 
-gulp.task('minify-css', function() {
-  return gulp.src('src/css/*.css')
+gulp.task('default', function(){
+	return gulp.src('src/css/*.css')
     .pipe(minifyCss({compatibility: 'ie8'}))
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(gulp.dest('dist/css'));
+})
